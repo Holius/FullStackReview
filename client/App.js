d158bbb6e49487e5be55f5ac5f3d4a0e1af11e7f
Justin Gilley <justindgilley@gmail.com>
2020-01-15 08:53:57 -0600

    working on file submission

--- client/App.js
+++ client/App.js
@@ -1,1 +1,30 @@
+import React from 'react';
+import Axios from 'axios';
+import Tasks from './Task';
 
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tasks: [],
+         }
+    }
+
+    getTasks() {
+        Axios.get('/Sager')
+        .then( (response) => {
+            console.log(response)
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>My To Do App</h1>
+                <input>
+
+                </input>
+            </div>
+        )
+    }
+}
--- server/server.js
+++ server/server.js
@@ -1,1 +1,14 @@
+var express = require('express');
+var app = express();
+var port = 3030
+var path = require('path');
 
+app.use(express.static(path.join(__dirname,'../dist')))
+
+app.get('/Saeger', (req,res) => {
+    
+})
+
+app.listen(port, () => {
+    console.log('Port ' + port + ' is running')
+})
